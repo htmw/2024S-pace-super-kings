55ee20f0f602c77255d9e9e46d9d6228039a23f1
Ankit Mhatre <ankitmhatre.mhatre9@gmail.com>
2024-04-03 15:27:23 -0400

    create socket chat server

--- Code/node-server/server.js
+++ Code/node-server/server.js
@@ -1,13 +1,57 @@
 // server.js
 
 require('dotenv').config(); // Load environment variables from .env file
-
+const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+const  {Server} = require('socket.io');
+
+
+/**
+ * SOCKET : Start
+ */
+const server = http.createServer(app);
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+  console.log('New client connected');
+
+  // Listen for user joining
+  socket.on('join', (userId) => {
+    console.log(`User ${userId} joined`);
+    
+    // Join a unique room based on user ID
+    socket.join(userId);
+  });
+
+  // Listen for incoming messages
+  socket.on('message', (data) => {
+    console.log('Received message:', data);
+    const { userId, message } = data;
+
+    // Broadcast the message to all clients in the same room (user's room)
+    io.to(userId).emit('message', message);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
+});
+
+
+/**
+ * Socket end
+ */
+
+
+
+
+
 
 // Middleware
 app.use(bodyParser.json());
@@ -36,10 +80,15 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
 // Routes
 const authRoutes = require('./routes/auth');
 const profileRoutes = require('./routes/profile');
+app.use('/profile', profileRoutes);
+const dashboardRoutes = require('./routes/dashboard');
 app.use('/auth', authRoutes);
 app.use('/profile', profileRoutes);
+app.use('/dashboard', dashboardRoutes);
 
 // Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
