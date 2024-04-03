f75646b1af8e3cb5ac82f0f873bbca5287dcc9aa
Ankit Mhatre <ankitmhatre.mhatre9@gmail.com>
2024-04-02 20:28:20 -0400

    update app.py and compose for diff envs

--- Code/pythonserver/app.py
+++ Code/pythonserver/app.py
@@ -1,4 +1,8 @@
+import os
 from flask import Flask
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 
@@ -7,4 +11,8 @@ def hello():
     return 'Hello, World!'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    environment = os.environ.get("ENVIRONMENT", "development")
+    debug = False if environment == "production" else True
+
+    port = int(os.environ.get("PORT", 3002))
+    app.run(host='0.0.0.0', port=port, debug=debug)
