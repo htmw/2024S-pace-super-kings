49c0fed45c402bbd73fe70c77e4d89f1e382335a
Ankit Mhatre <ankitmhatre.mhatre9@gmail.com>
2024-04-15 20:18:20 -0400

    add token check for user and balance api

--- routes/auth.js
+++ routes/auth.js
@@ -14,6 +14,7 @@ router.post('/signup', async (req, res) => {
 
     const existingUser = await User.findOne({ email });
 
+
     if (existingUser) {
       return res.status(400).json({ message: 'User already exists' });
     }
