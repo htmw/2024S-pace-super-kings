a54e101643a3cf84c3b5fa4c193a1cdeac3e5095
Ankit Mhatre <ankitmhatre.mhatre9@gmail.com>
2024-04-18 11:14:48 -0400

    change creds for heroku

--- Code/backend/node-server/constants.js
+++
@@ -1,8 +0,0 @@
-  const ALPHAVANTAGE_KEY = process.env.ALPHAVANTAGE_API;
-  const FMP_API = process.env.FNP_API_KEY;
-
-
-  module.exports = {
-    ALPHAVANTAGE_KEY,
-    FMP_API 
-  }
--- Code/frontend/src/Tv/js/functions.js
+++
@@ -1,9 +0,0 @@
-const sendMessage = (e)=>{
-    e.preventDefault();
-    let prompt_text = document.getElementById("prompt_text").value;
-    alert("Send Prompt to AI "+prompt_text);
-}
-
-
-
-export default sendMessage;
