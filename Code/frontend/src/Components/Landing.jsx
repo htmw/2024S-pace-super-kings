c0075fa3f0bd08997fea5ccd1ad7655a5cf0fc7d
kilaruchandana <manichandanakilaru@gmail.com>
2024-04-24 20:43:55 +0530

    added token in Landing.jsx to handle navigation

--- Code/frontend/src/Components/Landing.jsx
+++ Code/frontend/src/Components/Landing.jsx
@@ -25,6 +25,11 @@ const Landing = () => {
     return () => clearTimeout(timer);
   }, []);
     
+
+  const token = localStorage.getItem('token');
+  console.log(token);
+
+
   return (
     <>
     {/* <div>
@@ -165,7 +170,7 @@ const Landing = () => {
             InvestMate today!
           </p>
           <div className="getStarted" style={{ visibility: isVisible ? 'visible' : 'hidden' }}>
-       <Link to='/login'> <span>Get started  </span><FaLongArrowAltRight className="arrowRight" size={'30px'} /></Link>
+       { token ? <Link to='/features'> <span>Get started  </span><FaLongArrowAltRight className="arrowRight" size={'30px'} /></Link> : <Link to='/login'> <span>Get started  </span><FaLongArrowAltRight className="arrowRight" size={'30px'} /></Link> }
       </div>
         </div>
         <div className="videoBearBull">
